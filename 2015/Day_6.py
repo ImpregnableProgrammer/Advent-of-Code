a5615b70eb9df041c90596fc378761c29c6dbb5d
Name <kapur2001@gmail.com>
2016-12-04 15:54:21 -0800

    Update Day_6.py

--- 2015/Day_6.py
+++ 2015/Day_6.py
@@ -7,17 +7,12 @@ def First_Part(G):
    for c in range(N,-~J,1-2*(N>-~J)):H[i][c]=int(B and'01'[Y[1]=='on']or'01'[H[i][c]==0])
  print(sum(map(lambda f:f.count(1),H)))
  
-# Part Two; Currently Not Working
-def Second_Part(G,L=0):
- for i in G.split('\n'):Y=i.split();B=Y[0]=='turn';N,M=map(int,Y[[1,2][B]].split(','));J,K=map(int,Y[[3,4][B]].split(','));Q=abs(-~J-N)*abs(-~K-M);L=B and max(L+Q-2*Q*(Y[1]=='off'),0)or L+2*Q
- print(L)
- 
+# Part Two
 def Second_Part(G):
  H=eval(('['+'0,'*1000+'],')*1000)
  for i in G.split('\n'):
   Y=i.split();B=Y[0]=='turn';N,M=map(int,Y[[1,2][B]].split(','));J,K=map(int,Y[[3,4][B]].split(','))
   for i in range(M,-~K,1-2*(M>-~K)):
-   for c in range(N,-~J,1-2*(N>-~J)):H[i][c]=B and max(H[i][c]+(1-2*(Y[1]=='off')),0)or H[i][c]+2
- print(H)
+   for c in range(N,-~J,1-2*(N>-~J)):H[i][c]=max(H[i][c]+(1-2*(Y[1]=='off')),0)if B else H[i][c]+2
  print(sum(map(sum,H)))
    
