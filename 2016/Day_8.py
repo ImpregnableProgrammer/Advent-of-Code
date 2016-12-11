4de6bcbeaf50ba3576516f12c133c99b8eadae72
Name <kapur2001@gmail.com>
2016-12-10 17:25:30 -0800

    Update Day_8.py

--- 2016/Day_8.py
+++ 2016/Day_8.py
@@ -8,17 +8,14 @@ def Second_Part(f):
     Y=i.split()
     if Y[0]=='rect':
       K=map(int,Y[1].split('x'))
-      # print('rect',K)
       for h in range(K[1]):
         for m in range(K[0]):H[h][m]='#'
     else:
       if Y[1]=='row':
         V=int(Y[2].split('=')[-1])
-        # print('row',V,int(Y[-1]))
         for u in[1]*int(Y[-1]):H[V]=H[V][-1:]+H[V][:-1]
       else:
         V=int(Y[2].split('=')[-1])
-        print('column',V,int(Y[-1]))
         U=[H[i][V]for i in range(6)]
         for u in[1]*int(Y[-1]):U=U[-1:]+U[:-1]
         for c in range(6):H[c][V]=U[c]
