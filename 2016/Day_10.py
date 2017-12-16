a10cb79c477bf8ae49eb0413a495f4710835e29b
Name <kapur2001@gmail.com>
2017-12-16 15:37:03 -0800

    Update Day_10.py

--- 2016/Day_10.py
+++ 2016/Day_10.py
@@ -1,26 +1,65 @@
-# First Part
 import re
+
+# First Part
 def First_Part(s):
-  Bot_Dict = {}
-  while all(sorted(o,key=int)!=['61','17'] for o in Bot_Dict.values()):
-    for p in s.split('\n'):
-      p=re.sub('(?<=output )\d+',lambda k:str(-int(k.group(0))-1),p)
-      print(p)
+    Bot_Dict = {}
+    g=0
+    s=s.split('\n')
+    while 1:
+      p=re.sub('(?<=output )\d+',lambda k:str(-int(k.group(0))-1),s[g%len(s)])
       G=re.findall('-?\d+',p)
       if p[:3]=='bot' and G[0] in Bot_Dict.keys() and len(Bot_Dict[G[0]])>1:
+        if sorted(Bot_Dict[G[0]],key=int)==['17','61']:
+          print(G[0])
+          break
+        s.pop(g%len(s))
         if G[1] not in Bot_Dict.keys():
           Bot_Dict[G[1]]=[]
         if G[2] not in Bot_Dict.keys():
           Bot_Dict[G[2]]=[]
-        Bot_Dict[G[1]]+=len(Bot_Dict[G[1]])<2 and [min(Bot_Dict[G[0]],key=int)] or []
-        Bot_Dict[G[2]]+=len(Bot_Dict[G[1]])<2 and [max(Bot_Dict[G[0]],key=int)] or []
-        Bot_Dict[G[0]]=len(Bot_Dict[G[1]])>1 and [min(Bot_Dict[G[0]],key=int)] or len(Bot_Dict[G[2]])>1 and [max(Bot_Dict[G[0]],key=int)] or []
+        X=len(Bot_Dict[G[1]])
+        Y=len(Bot_Dict[G[2]])
+        Bot_Dict[G[1]]+=(G[1][0]=='-' or (G[1][0]!='-' and X<2)) and [min(Bot_Dict[G[0]],key=int)] or []
+        Bot_Dict[G[2]]+=(G[2][0]=='-' or (G[2][0]!='-' and Y<2)) and [max(Bot_Dict[G[0]],key=int)] or []
+        Bot_Dict[G[0]]=(G[1][0]!='-' and X>1) and [min(Bot_Dict[G[0]],key=int)] or (G[2][0]!='-' and Y>1) and [max(Bot_Dict[G[0]],key=int)] or []
       elif p[:5]=='value':
+        s.pop(g%len(s))
         if G[1] not in Bot_Dict.keys():
           Bot_Dict[G[1]]=[]
         Bot_Dict[G[1]]+=len(Bot_Dict[G[1]])<2 and [G[0]] or []
-      print(Bot_Dict)
-  print([o for o in Bot_Dict.keys() if sorted(Bot_Dict[o],key=int)==['61','17']])
-   
+      g+=1
+
+# Second Part
+def Second_Part(s):
+    Bot_Dict = {}
+    g=0
+    s=s.split('\n')
+    while 1:
+      p=re.sub('(?<=output )\d+',lambda k:str(-int(k.group(0))-1),s[g%len(s)])
+      G=re.findall('-?\d+',p)
+      if p[:3]=='bot' and G[0] in Bot_Dict.keys() and len(Bot_Dict[G[0]])>1:
+        s.pop(g%len(s))
+        if G[1] not in Bot_Dict.keys():
+          Bot_Dict[G[1]]=[]
+        if G[2] not in Bot_Dict.keys():
+          Bot_Dict[G[2]]=[]
+        X=len(Bot_Dict[G[1]])
+        Y=len(Bot_Dict[G[2]])
+        Bot_Dict[G[1]]+=(G[1][0]=='-' or (G[1][0]!='-' and X<2)) and [min(Bot_Dict[G[0]],key=int)] or []
+        Bot_Dict[G[2]]+=(G[2][0]=='-' or (G[2][0]!='-' and Y<2)) and [max(Bot_Dict[G[0]],key=int)] or []
+        Bot_Dict[G[0]]=(G[1][0]!='-' and X>1) and [min(Bot_Dict[G[0]],key=int)] or (G[2][0]!='-' and Y>1) and [max(Bot_Dict[G[0]],key=int)] or []
+      elif p[:5]=='value':
+        s.pop(g%len(s))
+        if G[1] not in Bot_Dict.keys():
+          Bot_Dict[G[1]]=[]
+        Bot_Dict[G[1]]+=len(Bot_Dict[G[1]])<2 and [G[0]] or []
+      g+=1
+      if len(s)<1:
+        j=1
+        for o in Bot_Dict.keys():
+          if 0>int(o)>-4:
+            j*=int(Bot_Dict[o][0])
+        print(j)
+        break
         
         
