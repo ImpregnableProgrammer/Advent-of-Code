f1a27a6509463fa54279fc9e438a37dac412a7af
Name <kapur2001@gmail.com>
2017-12-16 01:12:08 -0800

    Second part
    Finally finished part two of day 9 over a year later!

--- 2016/Day_9.py
+++ 2016/Day_9.py
@@ -4,3 +4,6 @@ First_Part=lambda k,v=[],re=__import__('re'),J=lambda u,re=__import__('re'):re.m
 # Part Two
   # Initial Attempt; Throws Segmentation Fault with huge inputs
   Initial_Second_Part=lambda k,re=__import__('re'),M=lambda i,re=__import__('re'):re.search('\(\d+x\d+\)',i):M(k)!=None and Second_Part(k[:M(k).span()[0]]+k[M(k).span()[1]:M(k).span()[1]+int(re.search('(?<=\()\d+',M(k).group()).group())]*int(re.search('(?<=x)\d+',M(k).group()).group())+k[M(k).span()[1]+int(re.search('(?<=\()\d+',M(k).group()).group()):])or len(k)
+# Final successfull attempt – FINALLY COMPLETED OVER ONE YEAR LATER!
+# Recursive, so make sure to set recursion limit higher than length of input
+D=lambda s,l=[[1,1]],c=0,re=__import__('re'):len(s)and (s[0]=='('and D(re.sub('^\(.*?\)','',s),[[1,1]]+[[o[0]-len(re.search('\(.*?\)',s).group(0)),o[1]]for o in l[1:]if o[0]>len(re.search('(?<=\()\d*',s).group(0))]+[[int(re.search('(?<=\()\d*',s).group(0)),int(re.search('(?<=x)\d*',s).group(0))*l[len(l)-1][1]]],c)or D(s[1:],[[1,1]]+[[o[0]-1,o[1]]for o in l[1:]if o[0]>1],c+l[len(l)-1][1]))or c
