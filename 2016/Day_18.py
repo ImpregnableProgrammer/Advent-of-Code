788c620078ff642219376ceaaa5d338c0249a6ee
Name <kapur2001@gmail.com>
2017-12-17 00:27:29 -0800

    Update Day_18.py

--- 2016/Day_18.py
+++ 2016/Day_18.py
@@ -9,7 +9,15 @@ def First_Part(n,s):
     h+=['.'+m+'.']
   print(c)
   
-# Second Part –– Same as first part
-
-  
+# Second Part –– Same as first part.
+# But no array used to store the many generated strings, for the sake of saving space.
+def Second_Part(n,s):
+  h='.'+s+'.'
+  Trap=['..^','^..','.^^','^^.']
+  c=s.count('.')
+  for i in range(n-1):
+    m=''.join(['.','^'][h[k:k+3]in Trap]for k in range(len(s)))
+    c+=m.count('.')
+    h='.'+m+'.'
+  print(c)
     
