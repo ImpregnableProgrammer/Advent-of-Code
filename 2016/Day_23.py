cb2a87208c6ddc35793a51638e557c86f8f192b9
Name <kapur2001@gmail.com>
2017-12-18 20:44:26 -0800

    Update Day_23.py

--- 2016/Day_23.py
+++ 2016/Day_23.py
@@ -1,23 +1,45 @@
 # First Part
-# Currently unfinished
 def First_Part(s):
-  Reg={'a':0,'b':0,'c':0,'d':0}
+  Reg={'a':7,'b':0,'c':0,'d':0}
   s=s.split('\n')
   i=0
   while i<len(s):
     l=s[i].split()
-    print(l)
-    if l[0]=='cpy':
-      if not l[2].isdigit():
-        Reg[l[2]]=l[1].isdigit()and int(l[1])or Reg[l[1]]
+    if l[0]=='cpy' and l[2].isalpha():
+      Reg[l[2]]=Reg[l[1]] if l[1].isalpha() else int(l[1])
     elif l[0]=='jnz':
-      i+=int(l[2])-1 if (l[1].isdigit() and int(l[1])) or Reg[l[1]] else 0
+      i+=(Reg[l[2]]-1 if l[2].isalpha() else int(l[2])-1) if (l[1].isalpha() and Reg[l[1]]) or (not l[1].isalpha() and int(l[1])) else 0
     elif l[0]=='tgl':
-      I=i+((l[1].isdigit() and int(l[1]))or Reg[l[1]])
-      if len(s[I].split())>2:
-        s[I]=s[I].replace('jnz','#').replace('cpy','jnz').replace('#','cpy')
-      else:
-        s[I]=s[i].replace('inc','#').replace('dec','inc').replace('tgl','inc').replace('#','dec')
+      I=i+(Reg[l[1]] if l[1].isalpha() else int(l[1]))
+      if I<len(s):
+        if len(s[I].split())>2:
+          s[I]=s[I].replace('jnz','#').replace('cpy','jnz').replace('#','cpy')
+        else:
+          s[I]=s[I].replace('inc','#').replace('dec','inc').replace('tgl','inc').replace('#','dec')
+    else:
+      Reg[l[1]]+=(-1)**(l[0]=='dec')
+    i+=1
+  print(Reg['a'])
+  
+# Second Part
+# Currently unfinished – Takes too long
+def Second_Part(s):
+  Reg={'a':12,'b':0,'c':0,'d':0}
+  s=s.split('\n')
+  i=0
+  while i<len(s):
+    l=s[i].split()
+    if l[0]=='cpy' and l[2].isalpha():
+      Reg[l[2]]=Reg[l[1]] if l[1].isalpha() else int(l[1])
+    elif l[0]=='jnz':
+      i+=(Reg[l[2]]-1 if l[2].isalpha() else int(l[2])-1) if (l[1].isalpha() and Reg[l[1]]) or (not l[1].isalpha() and int(l[1])) else 0
+    elif l[0]=='tgl':
+      I=i+(Reg[l[1]] if l[1].isalpha() else int(l[1]))
+      if I<len(s):
+        if len(s[I].split())>2:
+          s[I]=s[I].replace('jnz','#').replace('cpy','jnz').replace('#','cpy')
+        else:
+          s[I]=s[I].replace('inc','#').replace('dec','inc').replace('tgl','inc').replace('#','dec')
     else:
       Reg[l[1]]+=(-1)**(l[0]=='dec')
     i+=1
