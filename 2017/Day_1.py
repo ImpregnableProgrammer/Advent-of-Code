6bb55705fd1c661ae0f0a920b835301b7cbef1c6
Name <kapur2001@gmail.com>
2017-12-22 19:09:06 -0800

    Update Day_1.py

--- 2017/Day_1.py
+++ 2017/Day_1.py
@@ -1,4 +1,5 @@
-# First Part – input is a string
+# First Part – Input is a string.
 First_Part=lambda s:sum(int(g)for i,g in enumerate(s)if s[i]==s[(i+1)%len(s)])
 
-# Second Part
+# Second Part – Again, input is a string.
+Second_Part=lambda s:sum(int(g)for i,g in enumerate(s)if s[i]==s[(i+len(s)//2)%len(s)])
