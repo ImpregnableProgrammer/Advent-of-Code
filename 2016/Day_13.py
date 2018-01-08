7afcb12a037e406e3a5e595bd03d06ae820bfd07
Name <kapur2001@gmail.com>
2018-01-08 13:48:07 -0800

    Update Day_13.py

--- 2016/Day_13.py
+++ 2016/Day_13.py
@@ -1,38 +1,24 @@
 # First_Part
-def First_Part(n,x=1,y=1,xo=1,yo=1,c=0):
-  if x!=7 or y!=4:
-    if bin((x+y+1)*(x+y+4)-2*y+n).count('1')%2<1 and x+1!=xo:
-      #print(1)
-      First_Part(n,x+1,y,x,y,c+1)
-    if bin((x+y+1)*(x+y+4)-2*y+n-2).count('1')%2<1 and y+1!=yo:
-      #print(2)
-      First_Part(n,x,y+1,x,y,c+1)
-    if bin((x+y-1)*(x+y+2)-2*y+n).count('1')%2<1 and x-1!=xo:
-      #print(3)
-      First_Part(n,x-1,y,x,y,c+1)
-    if bin((x+y+1)*(x+y+2)-2*y+n+2).count('1')%2<1 and y-1!=yo:
-      #print(4)
-      First_Part(n,x,y-1,x,y,c+1)
-  else:
-    return c
-  
 def First_Part(n,xf,yf):
   i=0
-  l=[[1,1]]
+  l={(1,1):[[1,1]]}
   while 1:
-    u=[]
+    u={}
     for g in l:
-      x,y=g[0],g[1]
-      if x==xf and y==yf:
-        return i
-      if not bin((x+y+1)*(x+y+4)-2*y+n).count('1')%2:
-        u+=[[x+1,y]]
-      if not bin((x+y+1)*(x+y+4)-2*y-2+n).count('1')%2:
-        u+=[[x,y+1]]
-      if not bin((x+y-1)*(x+y+2)-2*y+n).count('1')%2:
-        u+=[[x-1,y]]
-      if not bin((x+y-1)*(x+y+2)-2*y+2+n).count('1')%2:
-        u+=[[x,y-1]]
+      xo,yo=g[0],g[1]
+      for q in l[g]:
+        x,y=q[0],q[1]
+        u[(x,y)]=[]
+        if x==xf and y==yf:
+          return i
+        if not bin((x+y+1)*(x+y+4)-2*y+n).count('1')%2:
+          u[(x,y)]+=[[x+1,y]]
+        if not bin((x+y+1)*(x+y+4)-2*y-2+n).count('1')%2:
+          u[(x,y)]+=[[x,y+1]]
+        if not bin((x+y-1)*(x+y+2)-2*y+n).count('1')%2 and x-1>-1 and x-1!=xo:
+          u[(x,y)]+=[[x-1,y]]
+        if not bin((x+y-1)*(x+y+2)-2*y+2+n).count('1')%2 and y-1>-1 and y-1!=yo:
+          u[(x,y)]+=[[x,y-1]]
     i+=1
     l=u
     
