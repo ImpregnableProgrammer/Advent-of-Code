e83a3441f0dab98f09bddf63f1482aab9b52afea
Name <kapur2001@gmail.com>
2018-01-08 14:12:39 -0800

    Update Day_13.py

--- 2016/Day_13.py
+++ 2016/Day_13.py
@@ -21,6 +21,31 @@ def First_Part(n,xf,yf):
           u[(x,y)]+=[[x,y-1]]
     i+=1
     l=u
+
+# Second Part – IN PROGRESS
+def Second_Part(n):
+  i=c=0
+  l={(1,1):[[1,1]]}
+  while i<=50:
+    u={}
+    for g in l:
+      xo,yo=g[0],g[1]
+      for q in l[g]:
+        x,y=q[0],q[1]
+        u[(x,y)]=[]
+        if not bin((x+y+1)*(x+y+4)-2*y+n).count('1')%2:
+          u[(x,y)]+=[[x+1,y]]
+        if not bin((x+y+1)*(x+y+4)-2*y-2+n).count('1')%2:
+          u[(x,y)]+=[[x,y+1]]
+        if not bin((x+y-1)*(x+y+2)-2*y+n).count('1')%2 and x-1>-1 and x-1!=xo:
+          u[(x,y)]+=[[x-1,y]]
+        if not bin((x+y-1)*(x+y+2)-2*y+2+n).count('1')%2 and y-1>-1 and y-1!=yo:
+          u[(x,y)]+=[[x,y-1]]
+    i+=1
+    l=u
+    c+=len(l)
+    print(c,len(l),l)
+  return c
     
     
     
