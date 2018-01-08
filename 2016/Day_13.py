0e4455893026d5949b20975fd17dd1da02325651
Name <kapur2001@gmail.com>
2018-01-08 14:20:47 -0800

    Update Day_13.py

--- 2016/Day_13.py
+++ 2016/Day_13.py
@@ -35,16 +35,18 @@ def Second_Part(n):
         u[(x,y)]=[]
         if not bin((x+y+1)*(x+y+4)-2*y+n).count('1')%2:
           u[(x,y)]+=[[x+1,y]]
+          c+=1
         if not bin((x+y+1)*(x+y+4)-2*y-2+n).count('1')%2:
           u[(x,y)]+=[[x,y+1]]
+          c+=1
         if not bin((x+y-1)*(x+y+2)-2*y+n).count('1')%2 and x-1>-1 and x-1!=xo:
           u[(x,y)]+=[[x-1,y]]
+          c+=1
         if not bin((x+y-1)*(x+y+2)-2*y+2+n).count('1')%2 and y-1>-1 and y-1!=yo:
           u[(x,y)]+=[[x,y-1]]
+          c+=1
     i+=1
     l=u
-    c+=len(l)
-    print(c,len(l),l)
   return c
     
     
