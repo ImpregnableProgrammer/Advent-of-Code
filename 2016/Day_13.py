acee73edb47525a19b825ddfa1c766055319a9b4
Name <kapur2001@gmail.com>
2018-01-08 12:32:39 -0800

    Update Day_13.py

--- 2016/Day_13.py
+++ 2016/Day_13.py
@@ -15,6 +15,26 @@ def First_Part(n,x=1,y=1,xo=1,yo=1,c=0):
       First_Part(n,x,y-1,x,y,c+1)
   else:
     return c
+  
+def First_Part(n,xf,yf):
+  i=0
+  l=[[1,1]]
+  while 1:
+    u=[]
+    for g in l:
+      x,y=g[0],g[1]
+      if x==xf and y==yf:
+        return i
+      if not bin((x+y+1)*(x+y+4)-2*y+n).count('1')%2:
+        u+=[[x+1,y]]
+      if not bin((x+y+1)*(x+y+4)-2*y-2+n).count('1')%2:
+        u+=[[x,y+1]]
+      if not bin((x+y-1)*(x+y+2)-2*y+n).count('1')%2:
+        u+=[[x-1,y]]
+      if not bin((x+y-1)*(x+y+2)-2*y+2+n).count('1')%2:
+        u+=[[x,y-1]]
+    i+=1
+    l=u
     
     
     
