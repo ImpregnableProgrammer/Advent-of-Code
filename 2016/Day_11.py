e8a7c5d071aec794bcf1217dbc80204fac916708
Name <kapur2001@gmail.com>
2018-01-14 20:33:52 -0800

    Update Day_11.py

--- 2016/Day_11.py
+++ 2016/Day_11.py
@@ -3,7 +3,8 @@ import re
 def First_Part(s):
   Z=lambda i:re.findall("a ([a-z-]+? (?:microchip|generator))",s.split('\n')[i])
   T=[[[Z(0),Z(1),Z(2),Z(3)]]*2]
-  print(T)
+  E=sum(len(Z(I)) for I in range(4))
+  #print("E =",E)
   P=0
   for Op in range(1):
     K=[]
@@ -13,18 +14,24 @@ def First_Part(s):
       for u in range(1,len(i)):
         for q in range(4):
           for z in range(len(i[u][q])):
-            O=i[u][q][z].replace(' microchip','*').replace('-compatible generator',' microchip').replace('*','-compatible generator')
+            O=i[u][q][z].replace('-compatible microchip','').replace(' generator','')
             if q+1<4:
               L=i[u][:q]+[i[u][q][:z]+i[u][q][z+1:]]+[i[u][q+1]+[i[u][q][z]]]+i[u][q+2:]
-              if O in i[u][q+1] and L!=Old:
+              #print(L)
+              if ('generator' in i[u][q][z] and all(n.replace('-compatible microchip',' generator') in i[u][q+1] for n in i[u][q+1] if 'microchip' in n and O not in n))\
+              or ('microchip' in i[u][q][z] and (not any(n for n in i[u][q+1] if 'generator' in n and O not in n) or O+' generator' in i[u][q+1]))\
+              and L!=Old:
                 U+=[L]
             if q-1>-1:
               L=i[u][:q-1]+[i[u][q-1]+[i[u][q][z]]]+[i[u][q][:z]+i[u][q][z+1:]]+i[u][q+1:]
-              if O in i[u][q-1] and L!=Old:
+              #print(L)
+              if ('generator' in i[u][q][z] and all(n.replace('-compatible microchip',' generator') in i[u][q-1] for n in i[u][q-1] if 'microchip' in n and O not in n))\
+              or ('microchip' in i[u][q][z] and (not any(n for n in i[u][q-1] if 'generator' in n and O not in n) or O+' generator' in i[u][q-1]))\
+              and L!=Old:
                 U+=[L]
         K+=[U]
-      print(K)
       T=K
+      #print("K =",K)
       P+=1
       
             
